27bcef07736da7d85a3c3b391c8021cc482ee26d
amin <AMTeam.web666@gmail.com>
2023-11-06 22:10:21 +0330

    add brand rout for (create .. index .. edit .. update .. store .. delete) and Admin dashboard index rout

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Market\BrandController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::prefix("admin")->namespace('Admin')->group(function () {
+    Route::get('/', function(){
+        return view('admin.index');
+    })->name('admin.dashboard.show');
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::prefix('brand')->group(function(){
+        Route::get('/',[BrandController::class , 'index'])->name('admin.brand.index');
+        Route::get('/create',[BrandController::class , 'create'])->name('admin.brand.create');
+        Route::get('/edit/{brand}',[BrandController::class , 'edit'])->name('admin.brand.edit');
+        Route::post('/store',[BrandController::class , 'store'])->name('admin.brand.store');
+        Route::patch('/update/{brand}',[BrandController::class , 'update'])->name('admin.brand.update');
+        Route::delete('/destroy/{brand}',[BrandController::class , 'destroy'])->name('admin.brand.destroy');
+    });
 });

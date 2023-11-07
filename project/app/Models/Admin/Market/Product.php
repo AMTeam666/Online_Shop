27ee7958d3a641254aea2837962b66aca0a953ef
amin <AMTeam.web666@gmail.com>
2023-11-07 16:38:48 +0330

    add fillable property .. add brand and category function

--- project/app/Models/Admin/Market/Product.php
+++ project/app/Models/Admin/Market/Product.php
@@ -4,8 +4,30 @@ namespace App\Models\Admin\Market;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $cast = ['product_image' => 'array'] ;
+    protected $fillable = 
+    ['product_name', 
+    'product_image', 
+    'price', 'stock', 
+    'age_range', 
+    'gender', 
+    'stock_status', 
+    'category_id', 
+    'user_id',
+     'brand_id'] ;
+
+     public function brand(){
+        return $this->belongsTo(Brand::class);
+     }
+     public function category(){
+        return $this->belongsTo(ProductCategory::class);
+     }
+
+
 }

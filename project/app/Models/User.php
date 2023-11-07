10a2f79689dd55bb8e4265b3efadebe67aec506b
amin <AMTeam.web666@gmail.com>
2023-11-07 16:37:04 +0330

    edit and complete fillable and add products function

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\Admin\Market\Product;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -20,9 +21,18 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'phone_number',
+        'code_melli',
+        'user_kind',
+        'status',
+        'store_address',
+        'store_lincess',
+        'address',
+        'post_code',
     ];
 
     /**
@@ -42,10 +52,11 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
     public function products (){
-        return $this->hasMany(Products::class);
+        return $this->hasMany(Product::class);
     }
 }

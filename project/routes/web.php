503c5fbf251556553f00988c53fcd08d19289651
amin <AMTeam.web666@gmail.com>
2023-11-07 16:40:32 +0330

    add category routs

--- project/routes/web.php
+++ project/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Market\BrandController;
+use App\Http\Controllers\Admin\Market\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ Route::prefix("admin")->namespace('Admin')->group(function () {
         return view('admin.index');
     })->name('admin.dashboard.show');
 
+    //brand
     Route::prefix('brand')->group(function(){
         Route::get('/',[BrandController::class , 'index'])->name('admin.brand.index');
         Route::get('/create',[BrandController::class , 'create'])->name('admin.brand.create');
@@ -25,5 +27,15 @@ Route::prefix("admin")->namespace('Admin')->group(function () {
         Route::post('/store',[BrandController::class , 'store'])->name('admin.brand.store');
         Route::patch('/update/{brand}',[BrandController::class , 'update'])->name('admin.brand.update');
         Route::delete('/destroy/{brand}',[BrandController::class , 'destroy'])->name('admin.brand.destroy');
+    });
+
+    //category
+    Route::prefix('category')->group(function(){
+        Route::get('/',[CategoryController::class , 'index'])->name('admin.category.index');
+        Route::get('/create',[CategoryController::class , 'create'])->name('admin.category.create');
+        Route::get('/edit/{category}',[CategoryController::class , 'edit'])->name('admin.category.edit');
+        Route::post('/store',[CategoryController::class , 'store'])->name('admin.category.store');
+        Route::patch('/update/{category}',[CategoryController::class , 'update'])->name('admin.category.update');
+        Route::delete('/destroy/{category}',[CategoryController::class , 'destroy'])->name('admin.category.destroy');
     });
 });

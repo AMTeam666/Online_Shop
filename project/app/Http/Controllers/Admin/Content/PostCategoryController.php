dbabe39ef0d6ee969886266cbf9bb540e7bf252b
amin <AMTeam.web666@gmail.com>
2023-11-09 12:22:20 +0330

    complete the methods in post and post_category controller

--- project/app/Http/Controllers/Admin/Content/PostCategoryController.php
+++ project/app/Http/Controllers/Admin/Content/PostCategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin\Content;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\PostCategoryRequest;
+use App\Models\Admin\Content\PostCategory;
 
 class PostCategoryController extends Controller
 {
@@ -12,7 +14,8 @@ class PostCategoryController extends Controller
      */
     public function index()
     {
-        //
+        $postCategories = PostCategory::orderBy("created_at","desc")->paginate(10);
+        return view("admin.Content.category.index", compact("postCategories"));
     }
 
     /**
@@ -20,15 +23,18 @@ class PostCategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view("admin.Content.category.create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostCategoryRequest $request)
     {
-        //
+        $inputs = $request->all();
+        $postCategory = PostCategory::create($inputs);
+        return redirect()->route('admin.content.category.index')->with('swal-success', 'دسته بندی شما با موفقیت ساخته شد');
+
     }
 
     /**
@@ -42,24 +48,27 @@ class PostCategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(PostCategory $postCategory)
     {
-        //
+        return view("admin.Content.category.edite", compact("PostCategory"));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PostCategoryRequest $request, PostCategory $postCategory)
     {
-        //
+        $inputs = $request->all();
+        $postCategory->update($inputs);
+        return redirect()->route('admin.content.category.index')->with('swal-success', 'دسته بندی شما با موفقیت ویرایش شد');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(PostCategory $postCategory)
     {
-        //
+        $result = $postCategory->delete();
+        return redirect()->route('admin.content.category.index')->with('swal-success', ' دسته بندی شما با موفقیت حذف شد');    
     }
 }
--- project/app/Http/Controllers/Admin/Content/PostController.php
+++ project/app/Http/Controllers/Admin/Content/PostController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin\Content;
 
-use App\Http\Controllers\Controller;
+use App\Models\Admin\Content\PostCategory;
 use Illuminate\Http\Request;
+use App\Models\Admin\Content\Post;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\PostRequest;
+use App\Http\Services\Image\ImageService;
 
 class PostController extends Controller
 {
@@ -12,7 +16,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy("created_at","desc")->paginate(10);
+        return view("admin.Content.post.index", compact("posts"));
     }
 
     /**
@@ -20,15 +25,32 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $postCategories = PostCategory::all();
+        return view("admin.Content.post.create", compact("postCategories"));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostRequest $request, ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+        if($request->hasFile('image'))
+        {
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'posts');
+            $result = $imageService->save($request->file('image'));
+
+            if($result === false)
+            {
+                return redirect()->route('admin.content.post.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
+            }
+
+            $inputs['image'] = $result;
+        }
+        $brand = Post::create($inputs);
+        return redirect()->route('admin.content.post.index')->with('swal-success','پست جدید شما با موفقیت ساخته شد');   
+    
+   
     }
 
     /**
@@ -42,24 +64,46 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Post $post)
     {
-        //
+        $postCategories = PostCategory::all();
+        return view('admin.Content.post.edit', compact('post','postCategories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PostRequest $request, Post $post, ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+        if($request->hasFile('image'))
+        {
+            if(!empty($post->image))
+            {
+                $imageService->deleteDirectoryAndFiles($post->image);
+            }
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'posts');
+            $imageService->setImageName('image');
+            $result = $imageService->save($request->file('image'));
+            if($result === false)
+            {
+                return redirect()->route('admin.content.post.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
+            }
+            $inputs['image'] = $result;
+        }
+
+        $post->update($inputs);
+        return redirect()->route('admin.content.post.index')->with('swal-success', 'پست شما با موفقیت ویرایش شد');
+    
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+        $result = $post->delete();
+        return redirect()->route('admin.content.post.index')->with('swal-success', ' پست شما با موفقیت حذف شد');
+  
     }
 }
